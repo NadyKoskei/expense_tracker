7f0b0cf76ea4cf91c14c65595fe3c511820fb577
NadyKoskei <cherotichnady@gmail.com>
2026-02-08 12:42:06 +0300

    creating a component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Components/Home";
 
-function App() {
+function Main() {
+  console.log("This is the main function.");
+
+  let name = "Nady";
+  console.log(name);
+
+  setTimeout(() => {
+    name = "To expense tracker";
+    console.log(name, 'after timeout');
+  }, 2000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="first-div">
+
+        {/* <Home name = {name}/>  */}
+
+        Hii {name}
+      </div>
+    </>
   );
 }
 
-export default App;
+export default Main;
