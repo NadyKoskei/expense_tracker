9965cb0b1099ae9034996f57856cd32fd821a91d
NadyKoskei <cherotichnady@gmail.com>
2026-02-10 21:17:44 +0300

    pages folder

---
+++ src/pages/report.js
@@ -0,0 +1,9 @@
+function Report(){
+    return (
+        <div>
+            <h1>Reports</h1>
+        </div>
+    )
+}
+
+export default Report;
---
+++ src/pages/transaction.js
@@ -0,0 +1,9 @@
+function Transaction() {
+  return (
+    <div>
+      <h1>Transactions</h1>
+    </div>
+  );
+}
+
+export default Transaction;
