9a2e33db7ff022e75050e27e74cc205aa6af83b4
NadyKoskei <cherotichnady@gmail.com>
2026-02-10 20:08:59 +0300

    conditional rendering

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+
 function Home(props){
 
-console.log(props, 'fromHome');
+// console.log(props, 'fromHome');
+
+// useEffect(() => {
+//     console.log('home component created');
+
+//     return () => {
+//         console.log('home component destroyed');
+//     }
+// }, []);    
+
 
 return(
     <div>
